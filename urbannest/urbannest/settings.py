a00fb00ce5fa879beb976915f4a41fc2e7e480ab
Chemlal M Iyed <iyedchemlal@gmail.com>
2025-09-20 15:03:36 +0100

    Update settings.py

--- urbannest/urbannest/settings.py
+++ urbannest/urbannest/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get("fdomin"),os.environ.get("bdomin")]
+ALLOWED_HOSTS = [os.environ.get("bdomin")]
 
 # Application definition
 INSTALLED_APPS = [
