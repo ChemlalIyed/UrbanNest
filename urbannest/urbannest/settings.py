f3e6e3fd8d6e44005a4f1961fef9663c6888a0a0
Chemlal M Iyed <iyedchemlal@gmail.com>
2025-09-20 15:36:22 +0100

    Update settings.py

--- urbannest/urbannest/settings.py
+++ urbannest/urbannest/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,8 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT= os.path.join(BASE_DIR,'static')
+STATIC_URL = "/static/"
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_URL='media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
